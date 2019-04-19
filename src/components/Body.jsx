a30288b2343b561447f849e8144f4ceab9d895f3
Student <example@example.com>
2019-04-19 09:36:41 -0700

    build sidebar component and nest it in body

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SideBar from './Sidebar';
+
+function Body(){
+  return(
+    <div className="bodyMain">
+      <p>Main Body</p>
+      <SideBar/>
+    </div>
+  )
+}
+
+export default Body;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SideBar(){
+  return(
+    <div className="sideBar">
+      <p>SideBar</p>
+    </div>
+  )
+}
+
+export default SideBar;

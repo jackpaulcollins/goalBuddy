cf045a59fc2879c9d09cdadc11c280d1c62668a1
jackpaulcollins <jackcollins@Jacks-MacBook-Pro.local>
2019-05-03 13:27:58 -0700

    configuring routing to mostly black profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+
 function Profile(){
   return(
     <div>
-      <Navbar/>
       <h1>Profile Page</h1>
     </div>
   )
 }
+
+export default Profile;
--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,8 @@ import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+import Profile from './components/Profile';
 
 const initialState = {
   userPosts: ['test post'],
@@ -29,7 +31,10 @@ const render = (Component) => {
   ReactDOM.render(
     <Provider store={store}>
       <HashRouter>
-        <Component/>
+        <Switch>
+          <Route exact path='/' component={Component} />
+          <Route path='/profile' component={Profile} />
+        </Switch>
       </HashRouter>
     </Provider>,
     document.getElementById('react-app-root')

a092761392d2b12a2c7da6e3d351e51482d98e7d
jackpaulcollins <jackcollins@Jacks-MacBook-Pro.local>
2019-05-18 15:23:12 -0700

    add styling to post detail component

--- src/components/feed/PostDetail.jsx
+++ src/components/feed/PostDetail.jsx
@@ -4,17 +4,19 @@ function PostDetail() {
 
   return(
     <div className="container section post-details">
-      <div className="card z-depth-0">
-        <div className="card-content">
-          <span className="card-authord">Post Author</span>
+      <div className="card blue-grey lighten-2">
+        <div className="card-content white-text">
+          <span className="card-title">Post Author</span>
           <p>Post Content......</p>
         </div>
-        <button>Like</button><p>Likes: </p>
-        <div>1st May, 2019</div>
+        <div className="card-action">
+          <button className="waves-effect waves-light btn">Like</button><p>Likes: </p>
+          <div>1st May, 2019</div>
+        </div>
       </div>
     </div>
   )
-  
+
 }
 
 export default PostDetail;

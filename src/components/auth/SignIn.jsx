b44e8f2fe48ee6a2aeb3def5bcd00b51451a8fd3
jackpaulcollins <jackcollins@Jacks-MacBook-Pro.local>
2019-05-18 15:39:46 -0700

    develop signIn component

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
 
-function SignIn(){
-  return(
-    <div>
-      <h1>Sign In Component</h1>
-    </div>
-  )
+class SignIn extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.signIn(this.state)
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Sign In</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" onChange={this.handleChange}/>
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input type="text" id="password" onChange={this.handleChange}/>
+          </div>
+          <div className="input-field">
+            <button className="waves-effect waves-light btn">Login</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default SignIn;

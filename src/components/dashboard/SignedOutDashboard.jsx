143bc9d099655bb7b60f97a3843f6b3f03935c30
Student <example@example.com>
2019-05-20 10:24:25 -0700

    style signed out homepage

--- src/components/dashboard/SignedOutDashboard.jsx
+++ src/components/dashboard/SignedOutDashboard.jsx
@@ -1,51 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../../scss/styles.scss';
 
-class SignedOutDashboard extends React.Component{
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      focus: null
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({
-      [e.target.id]: e.target.value
-    })
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    console.log(this.state)
-  }
-
-  render(){
-    return (
-      <div>
-        <h3>Welcome to Goal Buddy</h3>
-        <div className="container">
-          <form onSubmit={this.handleSubmit} className="white">
-            <h5 className="grey-text text-darken-3">Sign In</h5>
-            <div className="input-field">
-              <label htmlFor="email">Email</label>
-              <input type="text" id="email" onChange={this.handleChange}/>
-            </div>
-            <div className="input-field">
-              <label htmlFor="password">Password</label>
-              <input type="password" id="password" onChange={this.handleChange}/>
-            </div>
-            <div className="input-field">
-              <button className="waves-effect waves-light btn">Login</button><br/><br/>
-              <button className="waves-effect waves-light btn">Sign Up</button>
-            </div>
-          </form>
+function SignedOutDashboard(){
+  return (
+    <div className="dashboard-content">
+      <div className="card blue-grey">
+        <div className="card-content white-text homepage-card">
+          <h3>Welcome to Goal Buddy!</h3>
+          <p>Make Friends & Accomplish Goals With Them</p>
+          <div className="buttons"><Link to="/signin"><button className="waves-effect waves-light btn">Sign In!</button></Link><Link to="/signup"><button className="waves-effect waves-light btn">Sign Up</button></Link></div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 

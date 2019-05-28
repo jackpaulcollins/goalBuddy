ea9f7f5ccc376d6cf8c96ddc385ad0618b4b63f4
jackpaulcollins <jackcollins@Jacks-MBP.lan>
2019-05-28 14:47:10 -0700

    fix problem of goal partner returning as UID instead of user name

--- src/components/signedInProfile/PartneredGoalDetail.jsx
+++ src/components/signedInProfile/PartneredGoalDetail.jsx
@@ -10,31 +10,19 @@ import styles from '../../scss/styles.scss';
 function PartneredGoalDetail(props){
   const { goal, goals, users, auth } = props;
 
-  console.log(users)
+  const store = users;
+  console.log(goal)
 
-
-  const goalBuddyOne = goal.buddies[0]
-  const goalBuddyTwo = goal.buddies[1]
-  const signedInUser = auth.uid;
-  const goalAuthor = goal.authorId.uid;
-
-  const goalBuddyTwoName = function findPartneredUser(signedInUser, goalAuthor goalBuddyOne, goalBuddyTwo, users){
-    if signedInUser = goalAuthor{
-      users.find(function(goalBuddyTwo){
-        return goalBuddyTwo.firstName
-      } else {
-        return null;
-        }
-      }
-
-    console.log(goalBuddyTwoName)
-
-
+  function findUserName(user, store){
+    return users[user].firstName
+  }
 
   if (goal && users){
+    const goalBuddyOne = goal.buddies[0]
+    const goalBuddyTwo = goal.buddies[1]
     return(
       <div>
-        <h3>Your goal with {auth.uid = goal.authorId.uid ? <p>{goal.buddies[1]}</p> : <p>{goal.buddies[0]}</p>}</h3>
+        <h3>Your goal with {auth.uid = goal.authorId.uid ? <p>{findUserName(goalBuddyTwo)}</p> : <p>{findUserName(goalBuddyOne)}</p>}</h3>
       </div>
     )
   } else {
@@ -51,7 +39,6 @@ const mapStateToProps = (state, ownProps) => {
   const id = ownProps.match.params.id;
   const goals = state.firestore.data.goals;
   const users = state.firestore.data.users;
-  console.log(users)
   const goal = goals ? goals[id] : null;
   return {
     goal: goal,

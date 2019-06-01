9f786feb328ab5069564120d0de6bfe0404e8a59
jackpaulcollins <jackcollins@Jacks-MBP.lan>
2019-06-01 15:03:35 -0700

    make progess cell stateful and add ability to click particular cell to change value of 'clicked' property within state

--- src/components/goals/GoalProgressCell.jsx
+++ src/components/goals/GoalProgressCell.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import styles from '../../scss/styles.scss';
 
-function GoalProgressCell(){
-  return(
-    <div className="cell">
-    </div>
-  )
+class GoalProgressCell extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: null
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    alert('clicked')
+    this.setState({
+      clicked: true
+      })
+    }
+
+
+  render(){
+    return(
+      <div className="cell" onClick={this.handleClick}>
+
+      </div>
+    )
+  }
 }
 
 export default GoalProgressCell;
